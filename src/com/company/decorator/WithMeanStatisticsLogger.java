592effa69312b5264d6288fd5d16c84234c1c266
19Pawel97 <19pawel9772@gmail.com>
2024-02-12 23:19:25 +0100

    ex8

--- src/com/company/decorator/WithMeanStatisticsLogger.java
+++ src/com/company/decorator/WithMeanStatisticsLogger.java
@@ -4,6 +4,16 @@ import java.util.List;
 
 public class WithMeanStatisticsLogger implements StatisticsLogger {
 
+    //Zadanie 7 - decorator
+    //Dla klasy ExecutionTimesBaseStatistics, wykorzystując interfejs StatisticsLogger, stwórz dwa dekoratory, które dodają następującą funkcjonalność:
+    //
+    //WithMeanStatisticsLogger, który wyświetla średnią wyników (wartości listy executionTimes w ExecutionTimesBaseStatistics) przed wyświetleniem poszczególnych wyników
+    //WithSummaryStatisticsLogger, która przed wyświetleniem poszczególnych wyników, wyświetla następujące statystyki:
+    //liczba rekordów
+    //suma
+    //wartość minimalna
+    //wartość maksymalna
+
     private final StatisticsLogger statisticsLogger;
 
     public WithMeanStatisticsLogger(final StatisticsLogger statisticsLogger) {
